0866a8abdbf970cc959a6acab2c951ffcae84127
Pradyumna <tech.daspk@gmail.com>
2022-06-19 16:22:11 +0100

    remove autowired as it's a single constructor

--- src/main/java/com/spring/batch/springbatchexample/controller/BatchController.java
+++ src/main/java/com/spring/batch/springbatchexample/controller/BatchController.java
@@ -7,7 +7,6 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +18,14 @@ public class BatchController {
 
     private Logger LOGGER = LoggerFactory.getLogger(BatchController.class);
 
-    @Autowired
     private JobLauncher jobLauncher;
 
-    @Autowired
     private Job job;
+
+    public BatchController(JobLauncher jobLauncher,Job job){
+        this.jobLauncher = jobLauncher;
+        this.job = job;
+    }
 
     @GetMapping("/load")
     public BatchStatus getBatchJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {

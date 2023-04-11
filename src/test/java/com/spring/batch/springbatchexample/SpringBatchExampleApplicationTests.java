79a724a18c9e8b9fcaf6ede5d7e1df82c3a15d25
Pradyumna <tech.daspk@gmail.com>
2023-04-11 21:44:22 +0100

    @Configuration Props added

--- src/test/java/com/spring/batch/springbatchexample/SpringBatchExampleApplicationTests.java
+++ src/test/java/com/spring/batch/springbatchexample/SpringBatchExampleApplicationTests.java
@@ -1,11 +1,11 @@
 package com.spring.batch.springbatchexample;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBatchExampleApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
